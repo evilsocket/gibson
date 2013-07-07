d4cff04a4e130ad1e320b4c8f085b26357e203d8
evilsocket <evilsocket@gmail.com>
2013-07-08 01:12:13 +0200

    configure.h.in fix for fragmentation computation accross platforms

--- src/zmalloc.h
+++ src/zmalloc.h
@@ -33,6 +33,8 @@
 #ifndef __ZMALLOC_H
 #define __ZMALLOC_H
 
+#include "configure.h"
+
 /* Double expansion needed for stringification of macro values. */
 #define __xstr(s) __str(s)
 #define __str(s) #s
