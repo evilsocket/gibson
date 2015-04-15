3e7f9dafe1a141cc422ab4ec8d1e0ddd32297bd2
evilsocket <evilsocket@gmail.com>
2015-04-15 14:39:52 +0300

    Fixed not terminated string bug

--- src/zmem.c
+++ src/zmem.c
@@ -183,7 +183,7 @@ void *zmalloc(size_t size) {
 		zmalloc_oom_handler(size);
 		return NULL;
 	}
-	
+
 #ifdef HAVE_MALLOC_SIZE
     zmem_incr_mem(zmalloc_size(ptr));
     return ptr;
@@ -329,7 +329,7 @@ size_t zmem_rss(void) {
 
     snprintf(filename,256,"/proc/%d/stat",getpid());
     if ((fd = open(filename,O_RDONLY)) == -1) return 0;
-    if (read(fd,buf,4096) <= 0) {
+    if (read(fd,buf,4095) <= 0) {
         close(fd);
         return 0;
     }
